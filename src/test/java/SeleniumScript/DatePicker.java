6a14686b86ff341f024d74950d8bce0885243059
Abdul <iamrahman300@gmail.com>
2024-09-19 14:40:22 +0530

    First commit

---
+++ src/test/java/SeleniumScript/DatePicker.java
@@ -0,0 +1,45 @@
+package SeleniumScript;
+
+import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DatePicker {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		
+try {
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		driver.get("https://jqueryui.com/datepicker/");		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		/*LocalDate CurrentDte= null;
+		CurrentDte=LocalDate.now();
+		System.out.println(CurrentDte);
+		
+		String date=CurrentDte.toString();
+		System.out.println(date);
+		
+		Thread.sleep(1000);
+		js.executeScript("document.getElementById('datepicker').value = arguments[0];", date);*/
+		WebElement Clk=driver.findElement(By.xpath("//input[@id=\"datepicker\"]"));
+		Clk.click();
+		js.executeScript("arguments[0].click()", Clk);
+}
+catch (Exception e) {
+	driver.close();
+}
+		
+		
+
+	}
+
+}
